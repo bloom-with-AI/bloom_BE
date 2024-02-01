3bc94f4e50d6d386334edb95b5b5390c72813a2b
Jaylin16 <jaylin6115@gmail.com>
2024-02-01 23:29:04 +0900

    feat: kakaoLogin add

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
+const { sequelize } = require("./models");
+
 const port = 8080;
+const app = express();
+
+app.use(cors());
+
+// //db연결
+// sequelize
+//   .sync({ force: false })
+//   .then(() => {
+//     console.log("데이터베이스 연결 성공");
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
+
+//main page 연결 테스트용
+app.get("/", (req, res) => {
+  return res.send("Hello");
+});
+
+const userRouter = require("./api/routes/user");
+app.use("/login", [userRouter]);
+
+// app.use("/user", [userRouter]);
+
+const searchRouter = require("./api/routes/search");
+app.use("/search", [searchRouter]);
 
 app.listen(port, () => {
   console.log(`${port}서버에 연결되었습니다.`);
-});
-
-app.get("/", (req, res) => {
-  return res.send("Hello");
 });
