2827f774143ddce2b359c0a8cc457b2a43e091ba
jaylin <jaylin6115@gmail.com>
2024-02-16 02:57:20 +0900

    feat: naverLogin add

--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -5,4 +5,7 @@ const userController = require("../controllers/userController");
 //카카오 인가코드로 로그인 및 신규 회원가입
 router.get("/kakao/callback", userController.kakaoCallback);
 
+//네이버 인가코드로 로그인 및 신규 회원가입
+router.get("/naver/callback", userController.naverCallback);
+
 module.exports = router;
--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,10 @@
 const dbConnection = require("../utils/db");
 const { v4: UUID } = require("uuid");
 
-const findOneByKakaoId = async (kakaoId) => {
+const findOneBySocialId = async (socialId, provider) => {
   const query = `SELECT * FROM socialLogin s
                   inner join users u on s.user_id = u.user_id
-                  WHERE provider = 'kakao' AND provider_id = ${kakaoId};`;
+                  WHERE provider = '${provider}' AND provider_id = '${socialId}';`;
   const userInfo = await doQuery(query);
 
   return userInfo;
@@ -18,11 +18,29 @@ const createKakaoUser = async (userInfo) => {
   const newUser = await doQuery(createUserQuery);
 
   const createSocialUserQuery = `INSERT INTO socialLogin (provider_id, provider, access_token, profile_url, user_id) 
-  VALUES (${userInfo.kakaoId}, 'kakao', '${userInfo.accessToken}', '${userInfo.kakaoProfileImage}', ${newUser.insertId});`;
+  VALUES ('${userInfo.kakaoId}', '${userInfo.provider}', '${userInfo.accessToken}', '${userInfo.kakaoProfileImage}', ${newUser.insertId});`;
 
   const newKakaoUser = await doQuery(createSocialUserQuery);
 
   const addSocialIdQuery = `UPDATE users SET social_login_id = ${newKakaoUser.insertId} WHERE user_id = ${newUser.insertId};`;
+  await doQuery(addSocialIdQuery);
+
+  return Promise.resolve("회원가입이 완료되었습니다.");
+};
+
+const createNaverUser = async (userInfo) => {
+  const createUserQuery = `INSERT INTO users (email, name, password, gender) VALUES ('${
+    userInfo.naverEmail
+  }', '${userInfo.naverNickname}', '${UUID()}', '${userInfo.naverGender}');`;
+
+  const newUser = await doQuery(createUserQuery);
+
+  const createSocialUserQuery = `INSERT INTO socialLogin (provider_id, provider, access_token, refresh_token, profile_url, user_id) 
+  VALUES ('${userInfo.naverId}', '${userInfo.provider}', '${userInfo.accessToken}', '${userInfo.refreshToken}', '${userInfo.naverProfileImage}', ${newUser.insertId});`;
+
+  const newNaverUser = await doQuery(createSocialUserQuery);
+
+  const addSocialIdQuery = `UPDATE users SET social_login_id = ${newNaverUser.insertId} WHERE user_id = ${newUser.insertId};`;
   await doQuery(addSocialIdQuery);
 
   return Promise.resolve("회원가입이 완료되었습니다.");
@@ -40,4 +58,8 @@ const doQuery = (query) => {
   });
 };
 
-module.exports = { findOneByKakaoId, createKakaoUser };
+module.exports = {
+  findOneBySocialId,
+  createKakaoUser,
+  createNaverUser,
+};
