ed11ef5697bb67fa3ee22689dab5e518ad2a7a0e
Jaylin <jaylin6115@gmail.com>
2024-02-23 02:13:17 +0900

    fix: yml >> env 환경변수 적용 방식 수정

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,28 +1,14 @@
 const mysql = require("mysql2");
-const fs = require("fs");
-const path = require("path");
-const yaml = require("js-yaml");
-
-// YAML 파일 로드 및 파싱
-const yamlFilePath = path.join(__dirname, "..", "application.yml");
-let config;
-
-try {
-  const configFile = fs.readFileSync(yamlFilePath, "utf8");
-  config = yaml.load(configFile);
-} catch (error) {
-  console.error("Error reading YAML file:", error);
-}
 
 const dbConnection = mysql.createPool({
-  host: config.database.host,
-  port: config.database.port,
-  user: config.database.user,
-  password: config.database.password,
-  database: config.database.databaseName,
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
 });
 
 module.exports = dbConnection;
