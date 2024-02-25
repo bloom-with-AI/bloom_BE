8a69f05497a25051725f6d9c016765d31c1aeffc
Jaylin <jaylin6115@gmail.com>
2024-02-26 03:19:19 +0900

    fix: 환경변수 적용 방식 변경 (기존: yml -> 변경: env)

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,4 +1,5 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const dbConnection = mysql.createPool({
   host: process.env.DATABASE_HOST,
@@ -10,5 +11,11 @@ const dbConnection = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0,
 });
+
+if (process.env.DATABASE_NAME !== undefined) {
+  console.log("환경설정 변수가 성공적으로 읽히고 있습니다.");
+} else {
+  console.log("환경설정 변수가 정의되지 않았습니다.");
+}
 
 module.exports = dbConnection;
