c5c87b1c0ef9ec11e0ec831739f25f139c3381f4
Jaylin <jaylin6115@gmail.com>
2024-03-14 22:36:13 +0900

    update: auth관련 경로(path) 변경

--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 //카카오 인가코드로 로그인 및 신규 회원가입
-router.get("/kakao/callback", userController.kakaoCallback);
+router.get("/login/kakao/callback", userController.kakaoCallback);
 
 //네이버 인가코드로 로그인 및 신규 회원가입
-router.get("/naver/callback", userController.naverCallback);
+router.get("/login/naver/callback", userController.naverCallback);
 
 //로그아웃
-router.post("/:userId", userController.logout);
+router.post("/logout/:userId", userController.logout);
 
 module.exports = router;
